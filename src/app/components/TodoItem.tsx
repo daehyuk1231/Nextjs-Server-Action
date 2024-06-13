31822ca8e3de42c115040cd7064cc7a61afcd144
daehyuk1231 <155371052+daehyuk1231@users.noreply.github.com>
2024-06-13 11:25:54 +0900

    Add files via upload

---
+++ src/app/components/TodoItem.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import React from "react";
+import { Todo } from "./TodoList";
+import { deleteTodo } from "@/lib/actions";
+import Checkbox from "./Checkbox";
+
+const TodoItem = (todo: Todo) => {
+  return (
+    <form className='my-4 flex justify-between items-center'>
+      <label htmlFor='completed' className='text-2xl hover:underline'>
+        <Link href={`/edit/${todo.id}`}>{todo.title}</Link>
+      </label>
+      <div className='flex items-center gap-4'>
+        <Checkbox todo={todo}/>
+        <button
+        formAction={async () => {
+          "use server";
+          await deleteTodo(todo)
+        }}>X</button>
+      </div>
+    </form>
+  );
+};
+
+export default TodoItem;
